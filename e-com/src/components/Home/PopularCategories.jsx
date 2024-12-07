1293eea686f8cf338c17af814b7890deabc35ece
sefedemircan <sefedemircan@gmail.com>
2024-12-07 23:17:01 +0300

    Add iyzipay dependency, update PopularCategories component with new categories and images, and configure Vite server proxy

--- e-com/src/components/Home/PopularCategories.jsx
+++ e-com/src/components/Home/PopularCategories.jsx
@@ -2,39 +2,39 @@ const PopularCategories = () => {
   const categories = [
     {
       id: 1,
-      name: 'Kazak',
-      image: '/images/categories/Bluz_202212271854.webp',
-      link: '/kategori/kazak'
+      name: 'Elektronik',
+      image: 'https://images.unsplash.com/photo-1635870723802-e88d76ae324e?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: '/kategori/elektronik'
     },
     {
       id: 2,
-      name: 'İç Giyim',
-      image: '/images/categories/Dis_Giyim_Kadin_202301031347.webp',
-      link: '/kategori/ic-giyim'
+      name: 'Giyim',
+      image: 'https://images.unsplash.com/photo-1445205170230-053b83016050?q=80&w=500&h=500&fit=crop',
+      link: '/kategori/giyim'
     },
     {
       id: 3,
-      name: 'Sweatshirt',
-      image: '/images/categories/Sweat_202212302146.webp',
-      link: '/kategori/sweatshirt'
+      name: 'Ev & Mobilya',
+      image: 'https://images.unsplash.com/photo-1556228453-efd6c1ff04f6?q=80&w=500&h=500&fit=crop',
+      link: '/kategori/ev-mobilya'
     },
     {
       id: 4,
-      name: 'Pantolon',
-      image: '/images/categories/Pantolon_202212302146.webp',
-      link: '/kategori/pantolon'
+      name: 'Kozmetik',
+      image: 'https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?q=80&w=1487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: '/kategori/kozmetik'
     },
     {
       id: 5,
-      name: 'Dış Giyim',
-      image: '/images/categories/Dis_Giyim_Kadin_202301031347.webp',
-      link: '/kategori/dis-giyim'
+      name: 'Ayakkabı & Çanta',
+      image: 'https://images.unsplash.com/photo-1511556820780-d912e42b4980?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: '/kategori/ayakkabi-canta'
     },
     {
       id: 6,
-      name: 'Bluz',
-      image: '/images/categories/Sweat_202212302146.webp',
-      link: '/kategori/bluz'
+      name: 'Spor & Outdoor',
+      image: 'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?q=80&w=500&h=500&fit=crop',
+      link: '/kategori/spor-outdoor'
     }
   ];
 
